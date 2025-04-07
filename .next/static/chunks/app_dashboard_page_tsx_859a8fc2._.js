cb515f0367f6586a2901525bc87e6108916a9e5b
Adrian Zebrowski <adrian.k.zebrowski@gmail.com>
2025-04-07 21:44:48 +0200

    x

---
+++ .next/static/chunks/app_dashboard_page_tsx_859a8fc2._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_page_tsx_859a8fc2._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_page_tsx_859a8fc2._.js",
+  "chunks": [
+    "static/chunks/670a6_next_dist_1ac6ef86._.js"
+  ],
+  "source": "dynamic"
+});
